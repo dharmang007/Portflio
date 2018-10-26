5573cfa27500de0797acae7d8796a65b3ca32775
Dharmang <dharmangsolanki999@gmail.com>
2018-10-26 08:27:42 +0530

    Revert "PC update"
    This reverts commit d2b99dfa6682f6fc90e584bfad223f1e5e0748b2.

--- MyWebSite/MyProfile/views.py
+++ MyWebSite/MyProfile/views.py
@@ -6,13 +6,4 @@ def index(request):
     return render(request,'MyProfile/includes/home.html') # be careful to put this files in 'templates' folder
 
 def contact(request):
-    return render(request,'MyProfile/includes/contact.html', {'content':['If you would like to contact me, please email me','dharmangsolanki999@gmail.com']})
-
-def myArt(request):
-    return render(request,'MyProfile/includes/myArt.html')
-
-def myAcademics(request):
-    return render(request,'MyProfile/includes/myAcademics.html')
-
-def aboutMe(request):
-    return render(request,'MyProfile/includes/aboutMe.html')
+    return render(request,'MyProfile/includes/')
--- MyWebSite/MyWebSite/urls.py
+++ MyWebSite/MyWebSite/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import re_path,include
+from django.urls import path,include
 
 urlpatterns = [
-    re_path(r'^admin/', admin.site.urls),
-    re_path(r'^',include('MyProfile.urls')),
 
+    path('',include('MyProfile.urls')),
+    path('admin/', admin.site.urls)
 ]
