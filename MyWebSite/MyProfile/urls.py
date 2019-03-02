13ced1c5659a4c5b9817c6e699a8c66180ad393b
Dharmang Solanki <dharmangsolanki999@gmail.com>
2019-03-02 16:23:17 +0530

    Corrections

--- MyWebSite/MyProfile/urls.py
+++ MyWebSite/MyProfile/urls.py
@@ -2,10 +2,10 @@ from django.urls import re_path,include
 from . import views
 
 urlpatterns  = [
-    re_path(r'^home/',views.index,name= 'index'), # here I am returning the views, u can find this is views.py
+    re_path(r'^home/',views.index,name= 'home'), # here I am returning the views, u can find this is views.py
     re_path(r'^contact/',views.contact,name= 'contact'),
-    re_path(r'^myArt/',views.myArt),
-    re_path(r'^myAcademics/',views.myAcademics),
+    re_path(r'^myArt/',views.myArt,name ='art'),
+    re_path(r'^myAcademics/',views.myAcademics,name='academics'),
     re_path(r'^aboutMe/',views.aboutMe),
     re_path(r'^',views.index,name='index') # keep this element at last so that all the possibilities are tried out first 
     ]
